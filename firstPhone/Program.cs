7224098199fa60cfc4c641a2bd8335df0f8c950d
GavinChou <gavingavinamd@gmail.com>
2022-05-23 11:42:34 +0800

    在 local master 「不」 Pull就修改4

--- firstPhone/Program.cs
+++ firstPhone/Program.cs
@@ -3,3 +3,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("第一次commit");
 Console.WriteLine("手機在 local master 修改 1");
 Console.WriteLine("電話在 local master 修改 2");
+Console.WriteLine("在電話 local master 「不」 Pull就修改4");
